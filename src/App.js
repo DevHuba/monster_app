0abef29432db1a30d49734762e18fcd64eef84db
DevHuba <devhuba@gmail.com>
2022-10-26 11:38:18 +0300

    add search + mui library

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
+import { TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export const App = () => {
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => console.log(response.json()))
-      .then((users) =>
-        setMonsters(
-          () => {
-            return users;
-          },
-          () => {
-            console.log("monsters : ", monsters);
-          }
-        )
-      );
-  });
+      .then((res) => res.json())
+      .then((res) => {
+        setMonsters(res);
+        setFilteredMonsters(res);
+      });
+
+    console.log("monsters : ", monsters);
+  }, []);
+
+  const filterMonsters = (filterKey) => {
+    setFilteredMonsters(monsters.filter((item) => item.name.toLowerCase().includes(filterKey)));
+  };
 
   return (
     <div className="App">
-      <input />
-      {monsters.map((item) => {
+      <TextField
+        sx={{ m: 3 }}
+        id="outlined-basic"
+        variant="outlined"
+        label="Search monster"
+        onChange={(e) => {
+          const loweredItem = e.target.value.toLowerCase();
+          filterMonsters(loweredItem);
+        }}
+      />
+      {filteredMonsters.map((item) => {
         return (
           <div key={item.id}>
             <h1>{item.name}</h1>
